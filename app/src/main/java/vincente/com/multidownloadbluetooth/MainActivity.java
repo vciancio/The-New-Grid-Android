76c4cad0984f0a28dfed268fa3d60fb88118fe77
Vincente <vciancio@socaldevs.com>
2016-04-14 13:49:18 -0700

    uncommented the server startservice intent

--- app/src/main/java/vincente/com/multidownloadbluetooth/MainActivity.java
+++ app/src/main/java/vincente/com/multidownloadbluetooth/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, GattServerService.class);
-//                startService(intent);
+                startService(intent);
                 intent = new Intent(MainActivity.this, BluetoothLeService.class);
                 startService(intent);
             }
